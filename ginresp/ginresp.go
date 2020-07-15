5a67ae0b43a5f28514cce43fc5c4b24d8b5c40ed
Chat Won By m <chat.w@touchtechnologies.co.th>
2020-07-15 11:58:55 +0700

    fix

--- ginresp/ginresp.go
+++ ginresp/ginresp.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
+	validator "github.com/go-playground/validator/v10"
 
 	"github.com/touchtechnologies-product/goerror"
 )
